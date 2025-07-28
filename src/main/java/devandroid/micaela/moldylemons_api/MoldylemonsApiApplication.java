553102e2536cead5ed366ac0691a70848e0a15be
Micaela <micaelahimura@hotmail.com>
2025-07-28 18:05:17 -0300

    initial commit

---
+++ src/main/java/devandroid/micaela/moldylemons_api/MoldylemonsApiApplication.java
@@ -0,0 +1,13 @@
+package devandroid.micaela.moldylemons_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoldylemonsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoldylemonsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devandroid/micaela/moldylemons_api/MoldylemonsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package devandroid.micaela.moldylemons_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoldylemonsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
